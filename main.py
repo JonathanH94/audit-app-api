d22cffbe379921fac6b857bace295def7e616ac5
JonathanH94 <jono_125@yahoo.co.uk>
2025-06-30 15:49:35 +0100

    Edit submission endpoint is now finished and working.

--- main.py
+++ main.py
@@ -169,41 +169,57 @@ async def view_submission(response_id: int):
 @app.put('/edit_submission/{response_id}')
 async def edit_submission(response: Response, response_id: int):
     ##RESPONSE BLOCK###
-    async with async_engine.begin() as conn:
-       response_result = await conn.execute(text("SELECT r.team_id, r.completed_date FROM response r left join team t on r.team_id = t.team_id where r.response_id = :response_id"),
-                           {"response_id": response_id})
-       current_response = response_result.mappings().all()
-       current_data = current_response[0]
+    try:
+        async with async_engine.begin() as conn:
+           response_result = await conn.execute(text("SELECT r.team_id, r.completed_date FROM response r left join team t on r.team_id = t.team_id where r.response_id = :response_id"),
+                               {"response_id": response_id})
+           current_response = response_result.mappings().all()
+           current_data = current_response[0]
 
-       new_data = {"team_id": response.team_id, "completed_date": response.completed_date}
-       changes = {key: new_value for key, new_value in new_data.items() if key in current_data and current_data[key] != new_value }
+           new_data = {"team_id": response.team_id, "completed_date": response.completed_date}
+           changes = {key: new_value for key, new_value in new_data.items() if key in current_data and current_data[key] != new_value }
 
-       ##RESPONSE ANSWER BLOCK
-       ra_result = await conn.execute(text("SELECT ra.response_answer_id, ra.response_id, ra.question_id, ra.answer FROM response_answer ra where ra.response_id = :response_id"),
-                                    {"response_id": response_id})
-       current_ra = ra_result.mappings().all()
+           ##RESPONSE ANSWER BLOCK
+           ra_result = await conn.execute(text("SELECT ra.response_answer_id, ra.response_id, ra.question_id, ra.answer FROM response_answer ra where ra.response_id = :response_id"),
+                                        {"response_id": response_id})
+           current_ra = ra_result.mappings().all()
 
-       changes_ra = []
+           changes_ra = []
 
-       current_ra_dict = {item['question_id']: item for item in current_ra}
+           current_ra_dict = {item['question_id']: item for item in current_ra}
 
-       for item in response.response_answer:
-           if item.question_id in current_ra_dict:
-               if item.answer != current_ra_dict[item.question_id]['answer']:
-                   changes_ra.append({"response_answer_id": current_ra_dict[item.question_id]['response_answer_id'],
-                                      "question_id": current_ra_dict[item.question_id]['question_id'],
-                                      "answer": item.answer})
+           for item in response.response_answer:
+               if item.question_id in current_ra_dict:
+                   if item.answer != current_ra_dict[item.question_id]['answer']:
+                       changes_ra.append({"response_answer_id": current_ra_dict[item.question_id]['response_answer_id'],
+                                          "question_id": current_ra_dict[item.question_id]['question_id'],
+                                          "answer": item.answer})
 
-       ##RESPONSE SQL STATEMENT
+           ##RESPONSE SQL STATEMENT
 
-       update_stmt = "UPDATE response"
-       set_stmt = "SET"
+           if changes:
+               set_parts = [f"{key} = :{key}" for key, value in changes.items()]
+               set_clause = ', '.join(set_parts)
+               param_dict = changes
+               param_dict['response_id'] = response_id
+
+               query = f"UPDATE response SET {set_clause}, updated_at = datetime('now') WHERE response_id = :response_id"
+               print(query)
+               print(param_dict)
+               await conn.execute(text(query), param_dict)
+
+           ##RESPONSE ANSWER SQL STATEMENT
+           if changes_ra:
+               for item in changes_ra:
+                   query_ra = "UPDATE response_answer SET answer = :answer, updated_at = datetime('now')  WHERE response_answer_id = :response_answer_id"
+                   await conn.execute(text(query_ra), {"answer": item['answer'], "response_answer_id": item['response_answer_id']})
+        await async_engine.dispose()
+        return "Successfully updated"
+    except Exception as e:
+        await conn.rollback()
+        await async_engine.dispose()
+        raise HTTPException(status_code=500)
 
 
-       # conn.execute(text(""))
 
 
-
-
-       await async_engine.dispose()
-    return current_ra
