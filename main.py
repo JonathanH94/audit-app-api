cb90e8d431144e0e80199280a22480405a239f10
JonathanH94 <jono_125@yahoo.co.uk>
2025-06-30 14:53:03 +0100

    Added endpoints for user to be able to view all their submissions and view the individual details of the response. Endpoint started for editing a previous submission, but not finished yet.

--- main.py
+++ main.py
@@ -64,7 +64,6 @@ async def create_questionnaire(questionnaire: Questionnaire):
             return {"message": f"success - questionnaire {questionnaire.name} added"}
     except Exception as e:
         await conn.rollback()
-        print(e)
         raise HTTPException(status_code=500)
 
 """
@@ -87,23 +86,28 @@ async def select_questions(questionnaire_id: int):
 #CREATE AUDIT
 ####################
 """
-@app.post('/create_audit/{questionnaire_id}')
-async def create_audit(response: Response, questionnaire_id: int):
-    async with async_engine.begin() as conn:
+@app.post('/create_submission/{questionnaire_id}')
+async def create_submission(response: Response, questionnaire_id: int):
 
-        await conn.execute(text("INSERT INTO response (questionnaire_id, user_id, team_id, completed_date, created_at, updated_at) VALUES (:questionnaire_id, :user_id, :team_id, :completed_date, datetime('now'), datetime('now'))"),
-                           {"questionnaire_id": response.questionnaire_id, "user_id": response.user_id, "team_id": response.team_id, "completed_date": response.completed_date})
+    try:
+        async with async_engine.begin() as conn:
 
-        result = await conn.execute(text("select last_insert_rowid()"))
-        response_id = result.scalar()
+            await conn.execute(text("INSERT INTO response (questionnaire_id, user_id, team_id, completed_date, created_at, updated_at) VALUES (:questionnaire_id, :user_id, :team_id, :completed_date, datetime('now'), datetime('now'))"),
+                               {"questionnaire_id": response.questionnaire_id, "user_id": response.user_id, "team_id": response.team_id, "completed_date": response.completed_date})
+
+            result = await conn.execute(text("select last_insert_rowid()"))
+            response_id = result.scalar()
 
 
-        for item in response.response_answer:
-            await conn.execute(text("insert into response_answer (response_id, question_id, answer, created_at, updated_at) VALUES (:response_id, :question_id, :answer, datetime('now'), datetime('now'))"),
-                               {"response_id": response_id, "question_id": item.question_id, "answer": item.answer})
+            for item in response.response_answer:
+                await conn.execute(text("insert into response_answer (response_id, question_id, answer, created_at, updated_at) VALUES (:response_id, :question_id, :answer, datetime('now'), datetime('now'))"),
+                                   {"response_id": response_id, "question_id": item.question_id, "answer": item.answer})
 
-        await async_engine.dispose()
-    return {"success": "audit received"}
+            await async_engine.dispose()
+        return {"success": "audit submitted"}
+    except Exception as e:
+        await conn.rollback()
+        raise HTTPException(status_code=500)
 
 
 """
@@ -114,10 +118,92 @@ async def create_audit(response: Response, questionnaire_id: int):
 
 @app.delete('/delete_submission/{response_id}')
 async def delete_submission(response_id: int):
+    try:
+        async with async_engine.begin() as conn:
+            await conn.execute(text("PRAGMA foreign_keys = ON"))
+            await conn.execute(text("DELETE FROM response WHERE response_id = :response_id"), {"response_id": response_id})
+
+            await async_engine.dispose()
+
+        return {"submission deleted": response_id}
+    except Exception:
+        await conn.rollback()
+        raise HTTPException(status_code=500)
+
+
+"""
+####################
+#MY SUBMISSIONS
+####################
+"""
+@app.get('/my_submissions/{user_id}')
+async def my_submissions(user_id: int):
     async with async_engine.begin() as conn:
-        await conn.execute(text("PRAGMA foreign_keys = ON"))
-        await conn.execute(text("DELETE FROM response WHERE response_id = :response_id"), {"response_id": response_id})
+       result = await conn.execute(text("SELECT r.response_id, q.questionnaire_name, t.team_name, r.completed_date, r.updated_at FROM response r left join questionnaire q on r.questionnaire_id = q.questionnaire_id left join team t on r.team_id = t.team_id where r.user_id = :user_id"),
+                           {"user_id": user_id})
+       dict_result = result.mappings().all()
+       await async_engine.dispose()
+       return dict_result
 
-        await async_engine.dispose()
 
-    return {"submission deleted": response_id}
+"""
+####################
+#VIEW SUBMISSION
+####################
+"""
+@app.get('/my_submissions/view_submission/{response_id}')
+async def view_submission(response_id: int):
+    async with async_engine.begin() as conn:
+       result = await conn.execute(text("SELECT ra.response_answer_id, q2.questionnaire_name, t.team_name, ra.question_id, qc.category_name, q.question_name, ra.answer, ra.created_at, ra.updated_at "
+                                        "FROM response_answer ra inner join response r on ra.response_id = r.response_id left join questionnaire q2 on r.questionnaire_id = q2.questionnaire_id "
+                                        "left join team t on r.team_id = t.team_id  left join question q on ra.question_id = q.question_id left join question_category qc on q.category_id = qc.category_id where ra.response_id = :response_id"),
+                           {"response_id": response_id})
+       dict_result = result.mappings().all()
+
+       return dict_result
+"""
+####################
+#EDIT SUBMISSION
+####################
+"""
+@app.put('/edit_submission/{response_id}')
+async def edit_submission(response: Response, response_id: int):
+    ##RESPONSE BLOCK###
+    async with async_engine.begin() as conn:
+       response_result = await conn.execute(text("SELECT r.team_id, r.completed_date FROM response r left join team t on r.team_id = t.team_id where r.response_id = :response_id"),
+                           {"response_id": response_id})
+       current_response = response_result.mappings().all()
+       current_data = current_response[0]
+
+       new_data = {"team_id": response.team_id, "completed_date": response.completed_date}
+       changes = {key: new_value for key, new_value in new_data.items() if key in current_data and current_data[key] != new_value }
+
+       ##RESPONSE ANSWER BLOCK
+       ra_result = await conn.execute(text("SELECT ra.response_answer_id, ra.response_id, ra.question_id, ra.answer FROM response_answer ra where ra.response_id = :response_id"),
+                                    {"response_id": response_id})
+       current_ra = ra_result.mappings().all()
+
+       changes_ra = []
+
+       current_ra_dict = {item['question_id']: item for item in current_ra}
+
+       for item in response.response_answer:
+           if item.question_id in current_ra_dict:
+               if item.answer != current_ra_dict[item.question_id]['answer']:
+                   changes_ra.append({"response_answer_id": current_ra_dict[item.question_id]['response_answer_id'],
+                                      "question_id": current_ra_dict[item.question_id]['question_id'],
+                                      "answer": item.answer})
+
+       ##RESPONSE SQL STATEMENT
+
+       update_stmt = "UPDATE response"
+       set_stmt = "SET"
+
+
+       # conn.execute(text(""))
+
+
+
+
+       await async_engine.dispose()
+    return current_ra
